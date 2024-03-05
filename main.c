77247032211d4674faac5257260ac237917210c8
StephKevin <kevinstephano55@gmail.com>
2024-03-05 21:32:57 +0100

    add procedure

--- main.c
+++ main.c
@@ -1,48 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-void triageSimple(int arr[] , int n)
+void permuter(int tab[], int i, int j)
 {
-    int i , j , temp;
-    for (i=0;i<n-1; i++)
+    int temp;
+    temp = tab[i];
+    tab[i] = tab[j];
+    tab[j] = temp;
+}
+void tester(int tab[], int i, int j)
+{
+    if(tab[i]>tab[j])
+        {permuter(tab, i, j);}
+}
+void implementer1( int tab[], int i, int j, int n)
+{
+    for(j=i+1; j<=n-1; j++)
     {
-        for(j=i+1; j<=n-1; j++)
-        {
-            if(arr[i]>arr[j])
-            {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-            else{}
-        }
+        tester(tab, i, j);
     }
 }
-void affichage(int arr[], int n)
+void implementer2(int tab[], int i, int n, int j)
+{
+    for(i=0;i<n-1; i++)
+        {implementer1(tab,i,j,n);}
+}
+void afficher(int tab[], int n)
 {
     int i;
     for(i=0; i<=n-1; i++)
     {
-        printf("%d",arr[i]);
+        printf("%d",tab[i]);
         printf("\n");
     }
 }
 int main()
 {
-    int *arr;
-    int n, i;
+    int *tab;
+    int n, i, j;
     printf("entrez la taille du tableau: ");
     scanf("%d",&n);
     printf("entrez les elements du tab:\n");
     for(i=0; i<=n-1; i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&tab[i]);
     }
     printf("tableau non trié:\n");
-    affichage(arr, n);
-    triageSimple(arr, n);
+    afficher(tab, n);
+    implementer2(tab,i,n,j);
     printf("tableau trié:\n");
-    affichage(arr, n);
+    afficher(tab, n);
     return 0;
 }
